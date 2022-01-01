92d533435b3b2248f1febfba845acf5bd9963396
jgzhang3397 <jgzhang1998@gmail.com>
2022-01-01 13:25:03 +0100

    commit

---
+++ day01_code/src/day01/demo02/Demo01Method.java
@@ -0,0 +1,12 @@
+package day01.demo02;
+
+public class Demo01Method {
+    public static void main(String[] args) {
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 20; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
