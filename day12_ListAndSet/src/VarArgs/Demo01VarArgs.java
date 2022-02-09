0f3ffce13e2ad1f6272b66c078e2344ce9190ecf
jgzhang3397 <jgzhang1998@gmail.com>
2022-02-09 12:14:35 +0100

    commit--variable Args

---
+++ day12_ListAndSet/src/VarArgs/Demo01VarArgs.java
@@ -0,0 +1,69 @@
+package VarArgs;
+
+/**
+ * 可变参数：是JDK1.5之后出现的新特性
+ * 使用前提：
+ *      当方法的参数列表数据类型确定，但是参数的个数不确定，就可以使用可变参数
+ * 使用格式：
+ *      修饰符   返回值类型   方法名(数据类型...变量名){}
+ * 可变参数的原理：
+ *      可变参数底层就是一个数组，根据传递参数个数不同，会创建不同长度的数组，来存储这些参数
+ *      传递的参数个数，可以是0个(不传递),1,2,...多个
+ */
+public class Demo01VarArgs {
+
+    public static void main(String[] args) {
+        //int i = add();
+        //int i = add(10);
+        int i = add(10,20);
+        System.out.println(i);
+        method(1,"aaa",'c',3.14,45.34,36.9);
+    }
+
+    /*
+        可变参数的注意事项：
+            1. 一个方法的参数列表，只能有一个可变参数
+            2. 如果方法的参数有多个，那么可变参数必须写在参数列表的末尾
+     */
+    /*public static void method(int...a, String...b)
+    {
+
+    }*/
+    /*public static void method(int a, String b, char c, double...d){
+
+    }*/
+
+    //可变参数终极写法
+    public static void method(Object...obj){
+
+    }
+
+    /*
+    定义一个方法，计算n个int类型整数的和
+        add(); 就会创建一个长度为0的数组，new int[0]
+        add(10); 就会创建一个长度为1的数组，new int[10]
+        add(10,20); 就会创建一个长度为2的数组，new int[2]
+        ...
+     */
+    public static int add(int...arr){
+//        System.out.println(arr);//[I@7dc36524 底层是个数组
+//        System.out.println(arr.length);//0,1,2
+        //定义初始化变量，记录累计求和
+        int sum = 0;
+        //遍历数组，获取数组的每个元素
+        for (int i : arr) {
+            //累加求和
+            sum += i;
+        }
+        return sum;
+    }
+
+    /*//定义一个方法，计算三个int类型整数的和
+    public static int add(int a, int b, int c){
+        return  a+b+c;
+    }
+    //定义一个方法，计算两个int类型整数的和
+    public static int add(int a, int b){
+        return  a+b;
+    }*/
+}
