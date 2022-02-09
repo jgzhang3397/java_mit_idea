87d6d4e3600a3c793e8b92853770c1888bb6746c
jgzhang3397 <jgzhang1998@gmail.com>
2022-02-09 11:44:38 +0100

    commit--Set & hashCode

---
+++ day12_ListAndSet/src/HashCode/Person.java
@@ -0,0 +1,9 @@
+package HashCode;
+
+public class Person extends Object{
+
+    @Override
+    public int hashCode() {
+        return 1;
+    }
+}
---
+++ day12_ListAndSet/src/Set/HashSetSaveString.md
@@ -0,0 +1,2 @@
+链接：  
+[HashSet集合存储不重复原理](https://www.bilibili.com/video/BV1uJ411k7wy?p=267)
