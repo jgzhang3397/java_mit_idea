c96b40be04ae34a6cbba0e5897c3046f9c7bc0c7
jgzhang3397 <jgzhang1998@gmail.com>
2022-02-28 10:56:36 +0100

    commit -- The advantages of Runnable

--- day15_Thread/src/runnable/Demo01Runnable.java
+++ day15_Thread/src/runnable/Demo01Runnable.java
@@ -15,6 +15,15 @@ package runnable;
  *          3.创建一个Runnable接口的实现类对象
  *          4.创建Thread类对象，构造方法中传递Runnable接口的实现类对象
  *          5.调用Thread类中的start方法，开启新的线程执行run方法
+ *
+ *      实现Runnable接口创建多线程程序的好处:
+ *          1.避免了单继承的局限性
+ *              一个类只能继承一个类，类继承了Thread类就不能继承其他的类
+ *              实现了Runnable接口，还可以继承其他的类，实现其他的接口
+ *          2.增强了程序的拓展性,降低了程序的耦合性(解耦)
+ *              实现Runnable接口的方式，把设置线程任务和开启新线程进行了分离(解耦)
+ *              实现类中，重写了run方法，用来设置线程任务
+ *              创建Thread类对象，调用start方法，用来开启新的线程
 
  */
 public class Demo01Runnable {
@@ -25,9 +34,10 @@ public class Demo01Runnable {
 
         //4.创建Thread类对象，构造方法中传递Runnable接口的实现类对象
         Thread thread = new Thread(run);
-
+        Thread thread1 = new Thread(new RunnableImpl2());//打印Hello World
         //5.调用Thread类中的start方法，开启新的线程执行run方法
-        thread.start();
+        //thread.start();
+        thread1.start();
 
         for (int i = 0; i < 20; i++) {
             System.out.println(Thread.currentThread().getName()+"--->"+i);
