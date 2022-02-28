e70e4ea1bf832c2245c169f44f66a1adbfccd521
jgzhang3397 <jgzhang1998@gmail.com>
2022-02-28 10:09:46 +0100

    commit -- getThreadName

---
+++ day15_Thread/src/getThreadName/Demo01GetThreadName.java
@@ -0,0 +1,24 @@
+package getThreadName;
+
+/**
+ * 线程的名称：
+ *      主线程: main
+ *      新线程: Thread-0
+ *             Thread-1
+ *             Thread-2
+ *             ....
+ */
+public class Demo01GetThreadName {
+
+    public static void main(String[] args) {
+        //创建Thread类的子类对象
+        MyThread myThread = new MyThread();
+        myThread.start();//Thread-0 | Thread[Thread-0,5,main]
+
+        new MyThread().start();//Thread-1 | Thread[Thread-1,5,main]
+        new MyThread().start();//Thread-2 | Thread[Thread-2,5,main]
+
+        //链式编程
+        System.out.println(Thread.currentThread().getName());//main
+    }
+}
